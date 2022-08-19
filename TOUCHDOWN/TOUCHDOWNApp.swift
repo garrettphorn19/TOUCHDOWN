a78a284a6a9f4fcd868b829d6a2cefdfc1332d21
Garrett Horn <garrett@sbfmail.com>
2022-08-19 07:30:07 -0400

    Initial Commit

---
+++ TOUCHDOWN/TOUCHDOWNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOUCHDOWNApp.swift
+//  TOUCHDOWN
+//
+//  Created by Garrett Horn on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct TOUCHDOWNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
